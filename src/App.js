fd84610bb3ede5d6fa267f1bbeb5f1f01f7589e1
joshua-paragoso <paragoso90@gmail.com>
2020-10-21 13:38:05 -1000

    first comment

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
 
+//first comment 
 function App() {
   return (
     <div className="App">
