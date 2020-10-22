4eb61dfde7bb0aa199ac1e63fd2b1495fb72d65f
joshua-paragoso <paragoso90@gmail.com>
2020-10-22 11:00:51 -1000

    changed counterSlice to userSlice. Added a firebase connectivity for login purpose

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyB-JHqV9NZnUBCY1wCamALrSvngSWa0LS0",
+    authDomain: "imessage-clone-8a209.firebaseapp.com",
+    databaseURL: "https://imessage-clone-8a209.firebaseio.com",
+    projectId: "imessage-clone-8a209",
+    storageBucket: "imessage-clone-8a209.appspot.com",
+    messagingSenderId: "785453929325",
+    appId: "1:785453929325:web:b8f7cdc901f0333f32c064",
+    measurementId: "G-22HG7QWDTF"
+  };
