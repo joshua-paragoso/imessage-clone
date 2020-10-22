90cf273e00ed0691186872f12eb27a29f6d4743f
joshua-paragoso <paragoso90@gmail.com>
2020-10-21 16:12:59 -1000

    cleaned up chat header

--- src/Chat.js
+++ src/Chat.js
@@ -1,9 +1,18 @@
 import React from 'react'
-
+import "./Chat.css";
 function Chat() {
     return (
         <div className="chat">
-            
+
+            {/*chat header*/}
+                <div className="chat__header">
+                    <h4>To: <span className="chat__name"> Josh </span></h4>
+                    <strong> Details</strong>
+                </div>
+
+            {/* chat messages*/}
+
+            {/*chat input*/}
         </div>
     )
 }
