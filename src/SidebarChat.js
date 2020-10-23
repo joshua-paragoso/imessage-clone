c8e4cf623a398a72ad3e903193fc79fefffa96cf
joshua-paragoso <paragoso90@gmail.com>
2020-10-23 12:09:46 -1000

    side bar now displays messages and time and date of when it was sent

--- src/SidebarChat.js
+++ src/SidebarChat.js
@@ -1,11 +1,26 @@
 import { Avatar } from '@material-ui/core';
 import { useDispatch } from "react-redux";
 import {setChat } from "./features/chatSlice";
-import React from 'react';
+import React, {useState} from 'react';
 import "./SidebarChat.css";
+import db from './firebase';
+import { useEffect } from "react";
 
 function SidebarChat({id, chatName}) {
     const dispatch = useDispatch();
+
+    //storage for message to show on side chat
+    const [chatInfo, setChatInfo] = useState([]);
+
+    //use effect
+    useEffect(() => {
+        db.collection("chats")
+        .doc(id)
+        .collection("messages").orderBy("timestamp", "desc")
+        .onSnapshot((snapshot) => 
+            setChatInfo(snapshot.docs.map((doc) => doc.data()))
+        );
+    }, [id])
 
     return (
         <div 
@@ -19,11 +34,11 @@ function SidebarChat({id, chatName}) {
         } 
         
         className="sidebarChat">
-            <Avatar/>
+            <Avatar src={chatInfo[0]?.photo}/>
             <div className="sidebarChat__info">
                 <h3>{chatName}</h3>
-                <p>last message sent...</p>
-                <small>timestamp</small>
+                <p>{chatInfo[0]?.message}</p>
+                <small>{new Date(chatInfo[0]?.timestamp?.toDate()).toLocaleString()}</small>
             </div>
         </div>
     )
